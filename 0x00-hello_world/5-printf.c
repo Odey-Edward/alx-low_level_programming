c8b1f188fb0c0026039ca2aaa2e67953e2860d67
Odey-Edward <edwardodey04@gmail.com>
2023-02-16 14:36:26 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Accept no input.
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
