5e327ce19fa2e1ef89e368fa5568d97841102a35
Odey-Edward <edwardodey04@gmail.com>
2023-02-16 16:35:24 -0800

    types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ int main(void)
 	printf("Size of an int: %ld bytes(s)\n", sizeof(int));
 	printf("Size of long int: %ld bytes(s)\n", sizeof(long int));
 	printf("Size of long long int: %ld bytes(s)\n", sizeof(long long int));
-	printf("Size of float: %ld bytes(s)\n", sizeof(float));
+	printf("Size of float: %ld bytes(s)", sizeof(float));
 
 	return (0);
 }
