a339219bd26240510ec7d0784f745990c0b47dcf
Odey-Edward <edwardodey04@gmail.com>
2023-02-18 10:56:22 -0800

    putchar numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,31 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
 
 /**
-* print - Entry point
-*
-* num: num
-*/
-void print(long num)
-{
-	if (num != 0)
-	{
-		print(num / 10);
-		putchar((num % 10) + '0');
-	}
-}
-
-/**
-* main - Entry point
+* main - Enty point
 *
 * Return: 0 (success)
 */
 int main(void)
 {
-	long numbers = 123456789;
+	int i;
 
-	print(numbers);
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
 	putchar('\n');
 
 	return (0);
