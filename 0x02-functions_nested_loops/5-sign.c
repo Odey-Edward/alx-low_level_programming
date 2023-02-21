63f846d89d9ba2024a8ad0bf5f3bb64b4e9897b4
Odey-Edward <edwardodey04@gmail.com>
2023-02-21 10:35:09 -0800

    sign 3

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,7 +11,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
 	}
 	else if (n == 0)
@@ -21,7 +21,7 @@ int print_sign(int n)
 	}
 	else if (n < 0)
 	{
-		_putchar('-');
+		_putchar(45);
 		return (-1);
 	}
 }
