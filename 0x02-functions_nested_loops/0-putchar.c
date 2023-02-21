b1aab3ede8371e14581a64511256b14fdda1dcff
Odey-Edward <edwardodey04@gmail.com>
2023-02-21 04:15:57 -0800

    added 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: 0 (Success)
+*/
+	int _putchar(void)
+	{
+		putchar('_');
+		putchar('p');
+		putchar('u');
+		putchar('t');
+		putchar('c');
+		putchar('h');
+		putchar('a');
+		putchar('r');
+		putchar('\n');
+
+		return (0);
+	}
+int main(void)
+{
+	_putchar();
+
+	return (0);
+}
