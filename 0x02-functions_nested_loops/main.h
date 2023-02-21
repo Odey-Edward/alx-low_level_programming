03d9ec82ce0d0a8a361ed1a217622da742cb01d8
Odey-Edward <edwardodey04@gmail.com>
2023-02-21 04:10:57 -0800

    main header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+int _putchar(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
