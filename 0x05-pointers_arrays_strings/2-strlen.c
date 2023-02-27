e767872c50123b1b3d45a7e384a7b9f6d526d5db
Odey-Edward <edwardodey04@gmail.com>
2023-02-27 12:11:40 -0800

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _strlen - count length of string
+* @s: pointer
+* Return: count
+*/
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (count != '\0')
+	{
+		count++;
+	}
+
+	return (count);
+}
