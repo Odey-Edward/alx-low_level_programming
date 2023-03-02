07cbad11e06b2a725911a20bb003f796525f27df
Odey-Edward <edwardodey04@gmail.com>
2023-03-01 18:33:34 -0800

    To upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* string_toupper - convert lowercase alphabet to uppercase
+* @n: string
+* Return: n (uppercase)
+*/
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = n[i] - 32;
+		}
+		i++;
+	}
+	return (n);
+}
