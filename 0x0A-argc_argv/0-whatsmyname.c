c2c2bada95a5e3e7620e38c2fbe0db7cb0b1cb7a
Odey-Edward <edwardodey04@gmail.com>
2023-03-13 07:29:31 -0700

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+* @agrc: argument count
+* @agrv: argument vector
+* Return: 0 always (success)
+*/
+int main(int agrc, char *agrv[])
+{
+	printf("%s\n", agrv[0]);
+
+	return (0);
+}
