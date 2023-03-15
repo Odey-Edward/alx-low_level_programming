cc0a73abc48fad6e07b115cba98d4d35c219aa6f
Odey-Edward <edwardodey04@gmail.com>
2023-03-15 16:25:04 -0700

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* argstostr - concatenate string
+* @ac: argument count
+* @av: argument vector
+* Return: av (string)
+*/
+char *argstostr(int ac, char **av)
+{
+	int i;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+for (i = 0; i < ac; i++;)
+{
+	printf("%s\n", av[i]);
+}
+return (av);
+}
