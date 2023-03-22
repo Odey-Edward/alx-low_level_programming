2148b7809b8cf0b8f1cbe42859bbccfa79e1603f
Odey-Edward <122893706+Odey-Edward@users.noreply.github.com>
2023-03-22 18:44:46 +0100

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,9 +10,9 @@
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	long unsigned int i;
+	unsigned int i;
 
-	if (!array || !action)
+	if (!array || !action || !size)
 		return;
 
 	for (i = 0; i < size; i++)
