b5079f4099a797459e95fc9e60f79429d6e6cca6
Odey-Edward <122893706+Odey-Edward@users.noreply.github.com>
2023-04-03 01:16:46 +0100

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,13 +7,17 @@
 */
 void free_list(list_t *head)
 {
+	/* temporary memory */
 	char *tmp;
 	
+	/* check if head is NULL */
 	while (head)
 	{
+		/* move to the next link */
 		tmp = head->next;
 		free(head->str);
 		free(head);
+		/* assign head to the next link */
 		head = tmp;
 	}
 }
