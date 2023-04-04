5764e3cdd2f02ee1ac79757458228b4370832cc5
Odey-Edward <122893706+Odey-Edward@users.noreply.github.com>
2023-04-04 21:30:18 +0100

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,7 +9,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current;
 
-	if (*head == NULL)
+	if (head == NULL)
 	{
 		return;
 	}
@@ -21,5 +21,4 @@ void free_listint2(listint_t **head)
 		free(*head);
 		*head = current;
 	}
-	*head = NULL;
 }
