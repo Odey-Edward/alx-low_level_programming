ab9beff37716e8e012416ab5cd478af2df1a2516
Odey-Edward <edwardodey04@gmail.com>
2023-04-05 15:05:04 -0700

    fixed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,14 +30,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	while (i != (idx - 1))
 	{
-		if (tmp || tmp->next)
-		{
-			tmp = tmp->next;
-		}
-		else
-		{
+		if (!tmp || !tmp->next)
 			return (NULL);
-		}
+
+		tmp = tmp->next;
 		i++;
 	}
 	new_node->next = tmp->next;
