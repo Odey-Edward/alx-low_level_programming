bba696aa485520b46f530486336ceb5512ecb706
Odey-Edward <edwardodey04@gmail.com>
2023-04-09 08:31:18 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* create_file - creates a file.
+* @filename: the name of the file to create
+* @text_content: is a NULL terminated string to write to the file
+* Return:1 on success, -1 on failure
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, i = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_CREAT, 00600);
+		close(fd);
+
+		return (1);
+	}
+
+	fd = open(filename, O_CREAT | O_TRUNC | 2, 00600);
+	while (text_content[i] != '\0')
+	{
+		w = write(fd, &text_content[i], 1);
+		i++;
+	}
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
