40e108fe7b0cd7af576d853d519f6442f193b81a
Odey-Edward <edwardodey04@gmail.com>
2023-04-10 05:29:47 -0700

    fixed2

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,6 +3,25 @@
 #include <stdlib.h>
 
 /**
+* error_check - check for error
+* @fd: file to read from
+* @fd2: file to write to
+* @av: argument vector
+*/
+void error_check(int fd, int fd2, char **av)
+{
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	if (fd2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+}
+/**
 * main - program start from
 * @ac: argument count
 * @av: argument vector
@@ -11,7 +30,7 @@
 int main(int ac, char **av)
 {
 	int fd, fd2;
-	ssize_t c, w, r, c2;
+	ssize_t w, r, c;
 	char buffer[1024];
 
 	if (ac != 3)
@@ -21,21 +40,18 @@ int main(int ac, char **av)
 	}
 
 	fd = open(av[1], O_RDONLY);
-	r = read(fd, buffer, 1024);
-	if (fd == -1 || r == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
 	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	w = write(fd2, buffer, r);
-	if (fd2 == -1 || w == -1)
+	error_check(fd, fd2, av);
+
+	r = 1024;
+	while (r > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
+		r = read(fd, buffer, 1024);
+		w = write(fd2, buffer, r);
+		error_check(r, w, av);
 	}
-	c2 = close(fd);
-	if (c2 == -1)
+	c = close(fd);
+	if (c == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
@@ -43,7 +59,7 @@ int main(int ac, char **av)
 	c = close(fd2);
 	if (c == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
 		exit(100);
 	}
 return (0);
