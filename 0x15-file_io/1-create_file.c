45f311720c1e1eb272fe0f34f2f7f5c40f4d16a4
Edward-Odey <edwardodey04@gmail.com>
2023-05-09 06:04:40 -0700

    hello_world

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,8 +20,11 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	len = 0;
-	while (text_content[len])
-		len++;
+	if (text_content)
+	{
+		while (text_content[len])
+			len++;
+	}
 
 	wr = write(fd, text_content, len);
 	if (wr == -1)
