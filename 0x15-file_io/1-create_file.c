5c62c5cc214ba79014e3ab64a82c4137028a9175
Edward-Odey <edwardodey04@gmail.com>
2023-05-09 05:50:42 -0700

    strlen

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,11 +20,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	len = 0;
-	while (*text_content)
-	{
+	while (text_content[len])
 		len++;
-		text_content++;
-	}
 
 	wr = write(fd, text_content, len);
 	if (wr == -1)
