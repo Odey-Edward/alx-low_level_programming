92b6a138246c8fdef0cd3b945a44f76b5cdb3191
Edward_Odey <edwardodey04@gmail.com>
2023-08-18 10:49:45 -0700

    task_7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -46,6 +46,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->prev = current;
 	new->next = tmp;
 
-	tmp->prev = new;
+	if (tmp != NULL)
+		tmp->prev = new;
+
 	return (new);
 }
