60356a7b78b1ab0d438aed8a5e5fffd46742c358
Edward_Odey <edwardodey04@gmail.com>
2023-09-08 05:30:31 -0700

    created 0x1A-hash_tables directory

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create a hash
+ * @size: the size of the array
+ * Return: a pointer to the newly created hash table
+ * else NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *table;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+
+	table->size = size;
+
+	table->array = calloc(size, sizeof(hash_node_t));
+	if (!table->array)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+
+	return (table);
+}
