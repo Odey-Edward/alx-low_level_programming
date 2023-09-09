d72198b4967e54fb6e76eee9f80e29ef2adea2eb
Edward_Odey <edwardodey04@gmail.com>
2023-09-09 04:52:03 -0700

    fixed hash table print

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,20 +1,33 @@
 #include "hash_tables.h"
 
-/* hash_table_print - prints a hash table
+/**
+ * hash_table_print - prints a hash table
  * @ht: The hash table
  */
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
+	hash_node_t *tmp;
+	int sep;
 
 	if (!ht)
 		return;
 
+	sep = 0;
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i])
-			printf("'%s': '%s', ", ht->array[i]->key, ht->array[i]->value);
+		tmp = ht->array[i];
+
+		while (tmp)
+		{
+			if (sep)
+				printf(", ");
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			sep = 1;
+			tmp = tmp->next;
+
+		}
 	}
 	printf("}\n");
 }
--- 0x1A-hash_tables/5-main.c
+++ 0x1A-hash_tables/5-main.c
@@ -4,23 +4,30 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always EXIT_SUCCESS.
  */
 int main(void)
 {
-    hash_table_t *ht;
+	hash_table_t *ht;
 
-    ht = hash_table_create(1024);
-    hash_table_print(ht);
-    hash_table_set(ht, "c", "fun");
-    hash_table_set(ht, "python", "awesome");
-    hash_table_set(ht, "Bob", "and Kris love asm");
-    hash_table_set(ht, "N", "queens");
-    hash_table_set(ht, "Asterix", "Obelix");
-    hash_table_set(ht, "Betty", "Cool");
-    hash_table_set(ht, "98", "Battery Street");
-    hash_table_print(ht);
-    return (EXIT_SUCCESS);
+	ht = hash_table_create(30);
+	hash_table_set(ht, "USA", "Washington");
+	hash_table_set(ht, "France", "Paris");
+	hash_table_set(ht, "UK", "London");
+	hash_table_set(ht, "Spain", "Madrid");
+	hash_table_set(ht, "Germany", "Berlin");
+	hash_table_set(ht, "China", "Beijing");
+	hash_table_set(ht, "Russia", "Moscow");
+	hash_table_set(ht, "Bahrain", "Manama");
+	hash_table_set(ht, "Canada", "Ottawa");
+	hash_table_set(ht, "Brazil", "Brasilia");
+	hash_table_set(ht, "Sweden", "Stockholm");
+	hash_table_set(ht, "Iceland", "Reykjavik");
+	hash_table_set(ht, "Japan", "Tokyo");
+	hash_table_set(ht, "Australia", "Canberra");
+
+	hash_table_print(ht);
+	return (EXIT_SUCCESS);
 }
