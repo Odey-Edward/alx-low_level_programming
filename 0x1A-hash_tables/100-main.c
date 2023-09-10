8418b4a318fb5cf0c83891fc6447d7892560543a
Edward_Odey <edwardodey04@gmail.com>
2023-09-10 08:20:06 -0700

    100-sorted_hash_table.c

--- 0x1A-hash_tables/100-main.c
+++ 0x1A-hash_tables/100-main.c
@@ -10,26 +10,28 @@
  */
 int main(void)
 {
-    shash_table_t *ht;
+	shash_table_t *ht;
+	/*char *value;*/
 
-    ht = shash_table_create(1024);
-    shash_table_set(ht, "y", "0");
-    shash_table_print(ht);
-    shash_table_set(ht, "j", "1");
-    shash_table_print(ht);
-    shash_table_set(ht, "c", "2");
-    shash_table_print(ht);
-    shash_table_set(ht, "b", "3");
-    shash_table_print(ht);
-    shash_table_set(ht, "z", "4");
-    shash_table_print(ht);
-    shash_table_set(ht, "n", "5");
-    shash_table_print(ht);
-    shash_table_set(ht, "a", "6");
-    shash_table_print(ht);
-    shash_table_set(ht, "m", "7");
-    shash_table_print(ht);
-    shash_table_print_rev(ht);
-    shash_table_delete(ht);
-    return (EXIT_SUCCESS);
+	ht = shash_table_create(30);
+	shash_table_set(ht, "USA", "Washington");
+	shash_table_set(ht, "France", "Paris");
+	shash_table_set(ht, "UK", "London");
+	shash_table_set(ht, "Spain", "Madrid");
+	shash_table_set(ht, "Germany", "Berlin");
+	shash_table_set(ht, "China", "Beijing");
+	shash_table_set(ht, "Russia", "Moscow");
+	shash_table_set(ht, "Bahrain", "Manama");
+	shash_table_set(ht, "Canada", "Ottawa");
+	shash_table_set(ht, "Brazil", "Brasilia");
+	shash_table_set(ht, "Sweden", "Stockholm");
+	shash_table_set(ht, "Iceland", "Reykjavik");
+	shash_table_set(ht, "Japan", "Tokyo");
+	shash_table_set(ht, "Australia", "Canberra");
+
+	shash_table_print(ht);
+	printf("\n\n........................................................\n\n");
+
+	shash_table_print_rev(ht);
+	return (EXIT_SUCCESS);
 }
